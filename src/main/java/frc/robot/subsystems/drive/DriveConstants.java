f40cf96d7adeaf5f39b297043861d5795a8a9132
Jtwerp <114360534+Jtwerp@users.noreply.github.com>
2026-03-09 13:06:39 -0500

    Wheel offsets for real robot

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -30,10 +30,10 @@ public class DriveConstants {
       };
 
   // Zeroed rotation values for each module, see setup instructions
-  public static final Rotation2d frontLeftZeroRotation = new Rotation2d(2.746595);
-  public static final Rotation2d frontRightZeroRotation = new Rotation2d(-3.138194);
-  public static final Rotation2d backLeftZeroRotation = new Rotation2d(-1.366923);
-  public static final Rotation2d backRightZeroRotation = new Rotation2d(1.716796);
+  public static final Rotation2d frontLeftZeroRotation = new Rotation2d(-Math.PI / 2);
+  public static final Rotation2d frontRightZeroRotation = new Rotation2d(0.0);
+  public static final Rotation2d backLeftZeroRotation = new Rotation2d(Math.PI);
+  public static final Rotation2d backRightZeroRotation = new Rotation2d(Math.PI / 2);
 
   // Device CAN IDs
   public static final int pigeonCanId = CanIDs.kGyroID;
